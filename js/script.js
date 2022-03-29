51d8b344a02545998ceb9dac35b8d3c743f767a7
Roman Pchelkin <epos.91@mail.ru>
2022-03-29 16:09:10 +0300

    recipe-card-add

--- js/script.js
+++ js/script.js
@@ -1,13 +1,38 @@
 const ingridientsList = document.querySelector('.ingridients-list');
 const addIngridientBtn = document.querySelector('.add-ingridient-btn');
-let ingridientName = document.querySelector('.ingridient-input-name');
-let ingridientQuantity = document.querySelector('.ingridient-input-quantity');
+const ingridientName = document.querySelector('.ingridient-input-name');
+const ingridientQuantity = document.querySelector('.ingridient-input-quantity');
 const checklistBlock = document.querySelector('.checklist');
 const printList = document.querySelector('.print-list-items');
-const recipeForm = document.querySelector('.recipe-form')
-const checkForm = document.querySelector('.checklist-form')
+const recipeForm = document.querySelector('.recipe-form');
+const checkForm = document.querySelector('.checklist-form');
+const recipeCardTitle = document.querySelector('.recipe-card-title');
+const recipeCardSubtitle = document.querySelector('.recipe-card-subtitle');
+const recipeCardTitleInput = document.querySelector('.recipe-input-title');
+const recipeCardSubtitleInput = document.querySelector('.recipe-input-subtitle');
+const recipeCardBtn = document.querySelector('.recipe-card-btn');
 
 let counter = 0;
+
+recipeCardBtn.addEventListener('click', (e) => {
+	e.preventDefault();
+	if(recipeCardTitleInput.value != '' && recipeCardSubtitleInput != '') {
+		recipeCardTitle.innerHTML = recipeCardTitleInput.value;
+		recipeCardSubtitle.innerHTML = recipeCardSubtitleInput.value;
+		
+		recipeCardTitleInput.classList.toggle('input-hide');
+		recipeCardBtn.classList.toggle('input-hide');
+		recipeCardSubtitleInput.classList.toggle('input-hide');
+
+		ingridientName.classList.toggle('input-hide');
+		addIngridientBtn.classList.toggle('input-hide');
+		ingridientQuantity.classList.toggle('input-hide');
+		
+
+	} else {
+		alert('Введите название и описание')
+	}
+})
 
 addIngridientBtn.addEventListener('click', (e) => {
 	e.preventDefault();
@@ -59,6 +84,7 @@ addIngridientBtn.addEventListener('click', (e) => {
 					checkForm.style = 'display: block; background-color: #fff'
 					checklistBlock.removeChild(newBtnPrintList);
 					window.print();
+					// location.reload();
 				})
 
 			});
